7712959327203098aa48d8462902d03a23723a9c
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-09-05 15:37:56 +0000

    authentication routes defined

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./authentication/authentication.module')
+      .then(am => am.AuthenticationModule)
+  }
+];
