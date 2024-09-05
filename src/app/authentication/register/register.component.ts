1cb6820b4e188a9062984f6c3ad2f3272d70b640
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-09-05 15:41:49 +0000

    form builder setup to dispatch form actions to authenticate user

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Store} from "@ngrx/store";
+import {AuthenticationState} from "../store/auth.state";
+import {Register} from "../../model/auth";
+import {AuthenticationActions} from "../store/auth.actions";
+import {selectLoading} from "../store/auth.selectors";
 
 @Component({
   selector: 'register',
@@ -6,5 +12,35 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  public signupForm: FormGroup;
+  public loading = this.store.selectSignal(selectLoading);
+  public showPassword:  boolean = false;
 
+  constructor(private fb: FormBuilder, private store: Store<AuthenticationState>) {
+    this.signupForm = this.fb.group({
+      additionalProperties: this.fb.group({
+        phone_number: ['', Validators.required],
+        date_of_birth: ['', Validators.required]
+      }),
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  onRegisterSubmit(): void {
+    if (this.signupForm.valid) return
+
+    const user: Register = this.signupForm.value
+    this.store.dispatch(AuthenticationActions.register({user}))
+  }
+
+  toggleShowPassword(): void {
+    this.showPassword = !this.showPassword
+  }
+
+  public getControlValue(value: string) {
+    return this.signupForm.get(value)
+  }
 }
