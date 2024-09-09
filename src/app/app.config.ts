5ea0ec5a436d78f9e6b6e8a757f53b5e8d04ae66
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-09-09 16:30:33 +0000

    interceptors added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,21 +5,23 @@ import { routes } from './app.routes';
 import {provideState, provideStore} from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
-import {provideHttpClient} from "@angular/common/http";
-import {authenticationFeature} from "./authentication/store/auth.reducers";
-import * as AuthEffects from "./authentication/store/auth.effects"
-import * as Toast from "./store/toast.effects"
-import * as Spinner from "./store/spinner.effects"
-import * as Route from "./store/route.effects"
-import {provideSpinnerConfig} from "ngx-spinner";
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {authenticationFeature} from './authentication/store/auth.reducers';
+import * as AuthEffects from './authentication/store/auth.effects';
+import * as Toast from './store/toast.effects';
+import * as Spinner from './store/spinner.effects';
+import * as Route from './store/route.effects';
+import {provideSpinnerConfig} from 'ngx-spinner';
+import {authInterceptor} from './interceptors/auth/auth.interceptor';
+import {refreshTokenInterceptor} from './interceptors/refresh-token/refresh-token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor, refreshTokenInterceptor])),
     provideSpinnerConfig({type: 'ball-spin'}),
     provideStore(),
     provideState(authenticationFeature),
