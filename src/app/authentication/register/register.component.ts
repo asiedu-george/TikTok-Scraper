d5a2282bbca53be3d19acd8e86bdf18672c78b88
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-09-10 09:25:58 +0000

    form reset after error and success

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Store} from "@ngrx/store";
-import {AuthenticationState} from "../store/auth.state";
-import {Register} from "../../model/auth";
-import {AuthenticationActions} from "../store/auth.actions";
-import {selectLoading} from "../store/auth.selectors";
-import {phoneValidator} from "../../validators/phoneValidator";
-import {nameValidator} from "../../validators/nameValidator";
-import {emailValidator} from "../../validators/emailValidator";
-import {passwordValidator} from "../../validators/passwordValidator";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Store} from '@ngrx/store';
+import {AuthenticationState} from '../store/auth.state';
+import {Register} from '../../model/auth';
+import {AuthenticationActions} from '../store/auth.actions';
+import {selectLoading} from '../store/auth.selectors';
+import {nameValidator} from '../../validators/nameValidator';
+import {emailValidator} from '../../validators/emailValidator';
+import {passwordValidator} from '../../validators/passwordValidator';
 
 @Component({
   selector: 'register',
@@ -22,10 +21,6 @@ export class RegisterComponent {
 
   constructor(private fb: FormBuilder, private store: Store<AuthenticationState>) {
     this.signupForm = this.fb.group({
-      additional_properties: this.fb.group({
-        phone_number: ['', [Validators.required, phoneValidator()]],
-        date_of_birth: ['', Validators.required]
-      }),
       first_name: ['', [Validators.required, nameValidator()]],
       last_name: ['', [Validators.required, nameValidator()]],
       email: ['', [Validators.required, emailValidator()]],
@@ -34,17 +29,18 @@ export class RegisterComponent {
   }
 
   onRegisterSubmit(): void {
-    if (this.signupForm.invalid) return
+    if (this.signupForm.invalid) return;
 
-    const user: Register = this.signupForm.value
-    this.store.dispatch(AuthenticationActions.register({user}))
+    const user: Register = this.signupForm.value;
+    this.store.dispatch(AuthenticationActions.register({user}));
+    this.signupForm.reset();
   }
 
   public toggleShowPassword(): void {
-    this.showPassword = !this.showPassword
+    this.showPassword = !this.showPassword;
   }
 
   public getControl(value: string) {
-    return this.signupForm.get(value)
+    return this.signupForm.get(value);
   }
 }
