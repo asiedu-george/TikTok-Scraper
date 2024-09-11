4f2b7bf125088c5a1dfd8c7e8f7ba6542cc8a7d5
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-09-11 08:29:03 +0000

    canActivate guard implemented to check user access

---
+++ src/app/guard/auth.guard.ts
@@ -0,0 +1,42 @@
+import { CanActivateFn } from '@angular/router';
+import {inject} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {AuthenticationState} from '../authentication/store/auth.state';
+import {NgToastService} from 'ng-angular-popup';
+import {constants} from '../utils/contants';
+import {AuthenticationActions} from '../authentication/store/auth.actions';
+import {selectLoginToken} from '../authentication/store/auth.selectors';
+
+const getDependencies = () => {
+  return {
+    store: inject(Store<AuthenticationState>),
+    toast: inject(NgToastService)
+  };
+};
+
+const handleAuthorizedAccess = (store: Store<AuthenticationState>, toast: NgToastService) => {
+  toast.warning(
+    constants.unauthorizedAccess,
+    constants.accessDenied,
+    constants.toastDuration
+  );
+  store.dispatch(AuthenticationActions.logout());
+};
+
+const checkAuth = () => {
+  const { store, toast } = getDependencies();
+  const token = store.selectSignal(selectLoginToken);
+
+  if (token()) {
+    console.log('Authenticated', token());
+    return true;
+  } else {
+    console.log('Not Authenticated', token());
+    handleAuthorizedAccess(store, toast);
+    return false;
+  }
+}
+
+export const authGuard: CanActivateFn = () => {
+  return checkAuth();
+};
