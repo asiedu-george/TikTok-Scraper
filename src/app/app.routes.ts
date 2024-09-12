44629088b12b55129ac3819294e9ab3aaa0fa47d
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-09-12 14:38:47 +0000

    for you component route defined

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
+import {authGuard} from "./guard/auth.guard";
 
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./authentication/authentication.module')
       .then(am => am.AuthenticationModule)
+  },
+  {
+    path: 'foryou',
+    loadChildren: () => import('./trending/trending.module')
+      .then(tm => tm.TrendingModule),
+    canActivate: [authGuard]
   }
 ];
