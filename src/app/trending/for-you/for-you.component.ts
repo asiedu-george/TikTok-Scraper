1a38445ee5de2998184a76b5395f16025eebde5a
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-09-12 14:39:40 +0000

    For you component create

---
+++ src/app/trending/for-you/for-you.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'for-you',
+  templateUrl: './for-you.component.html',
+  styleUrl: './for-you.component.scss'
+})
+export class ForYouComponent {
+
+}
