2d85f4bf0eb27e0a510f6bbfcfbcff13c4aa5a93
George Kwabena Asiedu <george.asiedu@amalitech.com>
2024-09-16 17:15:49 +0000

    export keyword added

--- src/app/utils/teardown.ts
+++ src/app/utils/teardown.ts
@@ -1,7 +1,7 @@
 import {Subject} from 'rxjs';
 import {ChangeDetectorRef, inject, ViewRef} from '@angular/core';
 
-const onDestroy = () => {
+export const onDestroy = () => {
   const destroy$ = new Subject<void>();
   const viewRef = inject(ChangeDetectorRef) as ViewRef;
 
